fc395eb0a6a02f7365f4fb99ca400ad5f0650309
NIRVANALAN <maverickers@outlook.com>
2020-01-17 16:08:42 +0000

    init

---
+++ 739.每日温度.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=739 lang=cpp
+ *
+ * [739] 每日温度
+ *
+ * https://leetcode-cn.com/problems/daily-temperatures/description/
+ *
+ * algorithms
+ * Medium (57.35%)
+ * Likes:    206
+ * Dislikes: 0
+ * Total Accepted:    25.5K
+ * Total Submissions: 44.5K
+ * Testcase Example:  '[73,74,75,71,69,72,76,73]'
+ *
+ * 根据每日 气温 列表，请重新生成一个列表，对应位置的输入是你需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。
+ * 
+ * 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4,
+ * 2, 1, 1, 0, 0]。
+ * 
+ * 提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+ * 
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+using std::vector, std::endl;
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        
+    }
+};
+// @lc code=end
+
